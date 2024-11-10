ecfa70535c80b011937e49f3a81846eb564cd5fc
Ankur Kumar <94914292+Spidy127@users.noreply.github.com>
2024-11-10 13:20:50 +0530

    Create largest element in an array.cpp

---
+++ Array/largest element in an array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int largest(int arr[] , int n){
+    
+    int max = -1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void display(int arr[] ,  int n){
+    for(int i = 0; i < n; i++){
+        cout<<arr[i] << " ";
+    }
+    cout<<endl;
+}
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+   int maxElement = largest(arr, n);
+   
+    display(arr, n);
+    
+    cout<< maxElement <<endl;
+	
+	return 0;
+}
